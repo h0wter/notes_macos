5d5bc0a9b19e2b59f780ebca226d73e479649e02
h0wter <howter99@gmail.com>
2023-05-09 15:28:12 +0300

    feat: add components to App.js return statement

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { Head } from "./components/Head/Head";
+import { MainContainer } from "./App.styled";
+import { Sidebar } from "./components/Sidebar/Sidebar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          React
-        </a>
-      </header>
-    </div>
+    <MainContainer>
+      <Head />
+      <Sidebar />
+    </MainContainer>
   );
 }
 
---
+++ src/App.styled.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const MainContainer = styled.div`
+  width: 80vw;
+  height: 90vh;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: #f6f6f5;
+  border-radius: 5px;
+  overflow: hidden;
+`;
